20116f87fd358d86aa0b626636f9f2d246cbb43c
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-19 11:06:11 +0600

    Retrieving All Admin Data from the Database

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,12 +2,20 @@ import { type Request, type Response } from "express";
 import { adminService } from "./admin.service.js";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await adminService.getAllFromDB();
-  res.status(200).json({
-    success: true,
-    message: "Admins fetched successfully",
-    data: result,
-  });
+  try {
+    const result = await adminService.getAllFromDB(req.query);
+    res.status(200).json({
+      success: true,
+      message: "Admins fetched successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch admins",
+      error: error,
+    });
+  }
 };
 
 export const AdminController = {
