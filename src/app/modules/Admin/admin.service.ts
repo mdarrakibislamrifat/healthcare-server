84caf38e534ae020755235367f9eba22445faa27
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-19 11:26:35 +0600

    Optimization of Search Code

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,25 +1,34 @@
 import { PrismaClient } from "../../../generated/prisma/client.js";
-
+type AdminQueryParams = {
+  searchTerm?: string;
+};
 const prisma = new PrismaClient();
 
-const getAllFromDB = async (params: any) => {
-  const result = await prisma.admin.findMany({
-    where: {
+const getAllFromDB = async (params: AdminQueryParams) => {
+  const andConditions = [];
+  if (params.searchTerm) {
+    andConditions.push({
       OR: [
         {
           name: {
             contains: params.searchTerm || "",
-            mode: "insensitive",
+            mode: "insensitive" as const,
           },
         },
         {
           email: {
             contains: params.searchTerm || "",
-            mode: "insensitive",
+            mode: "insensitive" as const,
           },
         },
       ],
-    },
+    });
+  }
+
+  const whereConditions = andConditions.length ? { AND: andConditions } : {};
+
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
   });
   return result;
 };
