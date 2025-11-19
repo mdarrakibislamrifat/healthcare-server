806d9aafbb1f376d6efc2ea896535f9653f4d3a0
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-19 12:32:14 +0600

    electing Valid Data Fields Using the pick Function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,9 +1,26 @@
 import { type Request, type Response } from "express";
 import { adminService } from "./admin.service.js";
 
+const pick = (obj: any, keys: string[]) => {
+  const finalObj: any = {};
+
+  for (const key of keys) {
+    if (obj && Object.hasOwnProperty.call(obj, key)) {
+      finalObj[key] = obj[key];
+    }
+  }
+  return finalObj;
+};
+
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
-    const result = await adminService.getAllFromDB(req.query);
+    const filter = pick(req.query, [
+      "name",
+      "email",
+      "searchTerm",
+      "contactNumber",
+    ]);
+    const result = await adminService.getAllFromDB(filter);
     res.status(200).json({
       success: true,
       message: "Admins fetched successfully",
