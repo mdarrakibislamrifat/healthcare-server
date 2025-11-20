d4a1b5a08d58f2a62d9adf23e06e76644dd9b300
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-11-20 14:38:56 +0600

    Managing Not Found Routes

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import router from "./app/routes/index.js";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler.js";
 
 const app: Application = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,5 +23,20 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api/v1", router);
 app.use(globalErrorHandler);
+
+// handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API Not Found",
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: "API Not Found",
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
